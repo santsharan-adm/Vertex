f8e22313bbe06c261fa9dd4b474217f56ec69ab4
Sharan Sant <ssant@pmintl.net>
2026-01-02 12:42:31 +0530

    bug for selec grid

--- IPCSoftwares/IPCSoftware.app/Helpers/ListBoxSelectedItemsBehavior.cs
+++ IPCSoftwares/IPCSoftware.app/Helpers/ListBoxSelectedItemsBehavior.cs
@@ -9,92 +9,127 @@ namespace IPCSoftware.App.Helpers
 {
     public static class ListBoxSelectedItemsBehavior
     {
-        public static readonly DependencyProperty BindableSelectedItemsProperty =
-  DependencyProperty.RegisterAttached(
-        "BindableSelectedItems",
-    typeof(IList),
-         typeof(ListBoxSelectedItemsBehavior),
-            new PropertyMetadata(null, OnBindableSelectedItemsChanged));
+        private static readonly DependencyProperty IsUpdatingProperty =
+            DependencyProperty.RegisterAttached("IsUpdating", typeof(bool), typeof(ListBoxSelectedItemsBehavior));
 
- public static void SetBindableSelectedItems(DependencyObject element, IList value)
-    {
+        private static void SetIsUpdating(DependencyObject d, bool value) => d.SetValue(IsUpdatingProperty, value);
+        private static bool GetIsUpdating(DependencyObject d) => (bool)(d.GetValue(IsUpdatingProperty) ?? false);
+
+        private static readonly DependencyProperty CollectionChangedHandlerProperty =
+            DependencyProperty.RegisterAttached("CollectionChangedHandler", typeof(NotifyCollectionChangedEventHandler), typeof(ListBoxSelectedItemsBehavior));
+
+        private static void SetCollectionChangedHandler(DependencyObject d, NotifyCollectionChangedEventHandler? handler) => d.SetValue(CollectionChangedHandlerProperty, handler);
+        private static NotifyCollectionChangedEventHandler? GetCollectionChangedHandler(DependencyObject d) => d.GetValue(CollectionChangedHandlerProperty) as NotifyCollectionChangedEventHandler;
+
+        public static readonly DependencyProperty BindableSelectedItemsProperty =
+            DependencyProperty.RegisterAttached(
+                "BindableSelectedItems",
+                typeof(IList),
+                typeof(ListBoxSelectedItemsBehavior),
+                new PropertyMetadata(null, OnBindableSelectedItemsChanged));
+
+        public static void SetBindableSelectedItems(DependencyObject element, IList value)
+        {
             element.SetValue(BindableSelectedItemsProperty, value);
         }
 
-  public static IList GetBindableSelectedItems(DependencyObject element)
+        public static IList GetBindableSelectedItems(DependencyObject element)
         {
-   return (IList)element.GetValue(BindableSelectedItemsProperty);
-      }
+            return (IList)element.GetValue(BindableSelectedItemsProperty);
+        }
 
         private static void OnBindableSelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
- {
-            if (d is ListBox listBox)
+        {
+            if (d is not ListBox listBox)
+                return;
+
+            // detach old collection handler
+            if (e.OldValue is INotifyCollectionChanged oldCollection)
             {
-        // Unsubscribe from old collection
-          if (e.OldValue is INotifyCollectionChanged oldCollection)
- {
-             oldCollection.CollectionChanged -= (s, args) => OnCollectionChanged(listBox, args);
-      }
-
-              // Subscribe to new collection
-     if (e.NewValue is INotifyCollectionChanged newCollection)
-        {
-        newCollection.CollectionChanged += (s, args) => OnCollectionChanged(listBox, args);
-        }
-
-          listBox.SelectionChanged -= ListBox_SelectionChanged;
-       listBox.SelectionChanged += ListBox_SelectionChanged;
-        SyncSelectedItems(listBox);
+                var oldHandler = GetCollectionChangedHandler(listBox);
+                if (oldHandler != null)
+                {
+                    oldCollection.CollectionChanged -= oldHandler;
+                }
             }
-        }
 
-   private static void OnCollectionChanged(ListBox listBox, NotifyCollectionChangedEventArgs e)
-        {
-       // Re-sync the ListBox selection when the bound collection changes
+            // attach new collection handler
+            if (e.NewValue is INotifyCollectionChanged newCollection)
+            {
+                NotifyCollectionChangedEventHandler handler = (s, args) => SyncSelectedItems(listBox);
+                SetCollectionChangedHandler(listBox, handler);
+                newCollection.CollectionChanged += handler;
+            }
+            else
+            {
+                SetCollectionChangedHandler(listBox, null);
+            }
+
+            listBox.SelectionChanged -= ListBox_SelectionChanged;
+            listBox.SelectionChanged += ListBox_SelectionChanged;
+
             SyncSelectedItems(listBox);
         }
 
         private static void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-    if (sender is ListBox listBox)
+            if (sender is not ListBox listBox)
+                return;
+
+            var bindableSelectedItems = GetBindableSelectedItems(listBox);
+            if (bindableSelectedItems == null)
+                return;
+
+            if (GetIsUpdating(listBox))
+                return;
+
+            try
             {
-                var bindableSelectedItems = GetBindableSelectedItems(listBox);
-         if (bindableSelectedItems == null) return;
+                SetIsUpdating(listBox, true);
 
-                // Avoid re-entrancy: temporarily unsubscribe from collection changes
-       if (bindableSelectedItems is INotifyCollectionChanged notifyCollection)
-           {
-        // Use a flag or just clear and repopulate
-}
+                // remove deselected items
+                foreach (var item in e.RemovedItems)
+                {
+                    if (bindableSelectedItems.Contains(item))
+                        bindableSelectedItems.Remove(item);
+                }
 
-    bindableSelectedItems.Clear();
-        foreach (var item in listBox.SelectedItems)
-    {
-       bindableSelectedItems.Add(item);
-     }
+                // add newly selected items
+                foreach (var item in e.AddedItems)
+                {
+                    if (!bindableSelectedItems.Contains(item))
+                        bindableSelectedItems.Add(item);
+                }
+            }
+            finally
+            {
+                SetIsUpdating(listBox, false);
             }
         }
 
         private static void SyncSelectedItems(ListBox listBox)
-      {
-       var bindableSelectedItems = GetBindableSelectedItems(listBox);
-      if (bindableSelectedItems == null) return;
+        {
+            var bindableSelectedItems = GetBindableSelectedItems(listBox);
+            if (bindableSelectedItems == null)
+                return;
 
-     // Temporarily unsubscribe to avoid re-entrancy
-            listBox.SelectionChanged -= ListBox_SelectionChanged;
+            if (GetIsUpdating(listBox))
+                return;
 
-            listBox.SelectedItems.Clear();
-        foreach (var item in bindableSelectedItems.Cast<object>().ToList())
+            try
             {
-   // Only add if the item exists in the ListBox's ItemsSource
-     if (listBox.Items.Contains(item))
+                SetIsUpdating(listBox, true);
+                listBox.SelectedItems.Clear();
+                foreach (var item in bindableSelectedItems.Cast<object>())
                 {
-  listBox.SelectedItems.Add(item);
-        }
-      }
-
-        // Re-subscribe
-          listBox.SelectionChanged += ListBox_SelectionChanged;
+                    if (listBox.Items.Contains(item))
+                        listBox.SelectedItems.Add(item);
+                }
+            }
+            finally
+            {
+                SetIsUpdating(listBox, false);
+            }
         }
     }
 }
