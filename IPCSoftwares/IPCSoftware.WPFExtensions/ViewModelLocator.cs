5211379555e59ec87ae36a32d9563f3b3a0cf970
krishnagaud463 <krishnagaud463@gmail.com>
2026-03-19 12:13:17 +0530

    AboutView/Fixed

--- IPCSoftwares/IPCSoftware.WPFExtensions/ViewModelLocator.cs
+++ IPCSoftwares/IPCSoftware.WPFExtensions/ViewModelLocator.cs
@@ -1,5 +1,6 @@
 using IPCSoftware.Shared;
 using System;
+using System.Diagnostics;
 using System.Windows;
 
 namespace IPCSoftware.Common.WPFExtensions
@@ -27,10 +28,23 @@ namespace IPCSoftware.Common.WPFExtensions
         {
             if (e.NewValue is Type viewModelType)
             {
-                var viewModel = ServiceLocator.Current.GetService(viewModelType);
-                if (d is FrameworkElement element)
+                try
                 {
-                    element.DataContext = viewModel;
+                    var viewModel = ServiceLocator.Current.GetService(viewModelType);
+                    if (viewModel == null)
+                    {
+                        Debug.WriteLine($"[ViewModelLocator] ERROR: Could not resolve {viewModelType.FullName} from DI. Is it registered?");
+                        return;
+                    }
+
+                    if (d is FrameworkElement element)
+                    {
+                        element.DataContext = viewModel;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"[ViewModelLocator] ERROR resolving {viewModelType.FullName}: {ex.Message}");
                 }
             }
         }
