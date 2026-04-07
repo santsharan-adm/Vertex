4b6e5db0df0354db9928c8acd6c23ad16560aff7
sanjeevkumar72 <160614974+sanjeevkumar72@users.noreply.github.com>
2026-04-07 16:37:30 +0530

    UpdatedUI 07042026

---
+++ IPCSoftwares/IPCSoftware.App.Bending/Views/DashBoard1.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace IPCSoftware.App.Bending.Views
+{
+    public partial class DashBoard1 : UserControl
+    {
+        public DashBoard1()
+        {
+      
+            InitializeComponent();
+        }
+    }
+}
